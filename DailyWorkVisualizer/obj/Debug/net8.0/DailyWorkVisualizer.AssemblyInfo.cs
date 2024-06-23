727aff49b1454966bddba021facaf32e7fbf8384
smease12 <scmease93@gmail.com>
2024-06-22 21:08:42 -0400

    Change text on commit buttons

--- DailyWorkVisualizer/obj/Debug/net8.0/DailyWorkVisualizer.AssemblyInfo.cs
+++ DailyWorkVisualizer/obj/Debug/net8.0/DailyWorkVisualizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyWorkVisualizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c878eeeda799fce6709739c3cad74e91a3c199b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ddb12250440ed671eeffbd715a82392511a50bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyWorkVisualizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyWorkVisualizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
